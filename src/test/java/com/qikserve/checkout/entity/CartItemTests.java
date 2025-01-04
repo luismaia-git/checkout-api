fd3f3964dde016e0a1d0fe150bb060200380db22
maial <luismaia1407@gmail.com>
2025-01-03 21:14:20 -0300

    feat(tests) : refactor Cart/CartItem tests

--- src/test/java/com/qikserve/checkout/entity/CartItemTests.java
+++ src/test/java/com/qikserve/checkout/entity/CartItemTests.java
@@ -8,19 +8,20 @@ import com.qikserve.checkout.repository.cart.CartRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class CartItemTests {
     @Autowired
     private CartRepository cartRepository;
 
     @Autowired
     private CartItemRepository cartItemRepository;
-
 
     @Test
     public void shouldGetAllCartItemsSuccessfully() {
--- src/test/java/com/qikserve/checkout/factory/CartFactory.java
+++ src/test/java/com/qikserve/checkout/factory/CartFactory.java
@@ -8,14 +8,10 @@ import java.util.ArrayList;
 public class CartFactory {
 
     public static Cart makeCart() {
-        return makeCart(new Cart());
-    }
-
-    public static Cart makeCart(Cart override) {
         return Cart.builder()
-                .tenantId(override.getTenantId() != null ? override.getTenantId() : "default-tenant")
-                .status(override.getStatus() != null ? override.getStatus() : CartStatus.OPEN)
-                .items(override.getItems() != null ? override.getItems() : new ArrayList<>())
+                .tenantId("default-tenant")
+                .status(CartStatus.OPEN)
+                .items(new ArrayList<>())
                 .build();
     }
 }
